e34174808c3b8cc8a539012f7146e5667ce0e983
Shivaji Chalise <respond2shivaji@gmail.com>
2023-03-11 23:25:57 +0545

    made imagemodal responsive

--- frontend/src/styles/imageModalStyles.js
+++ frontend/src/styles/imageModalStyles.js
@@ -5,21 +5,30 @@ const imageModalStyles = makeStyles()((theme) => {
         modalWrapper: {
         },
         modal: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
         },
         modalBox: {
             backgroundColor: '#f2f2f2',
             borderRadius: 14,
-            width: '60%',
             position: 'absolute',
             top: '50%',
             left: '50%',
             transform: 'translate(-50%, -50%)',
-            padding: theme.spacing(1, 1, 1, 1),
+            // padding: theme.spacing(1, 1, 1, 1),
             display: 'flex',
             justifyContent: 'space-between',
             alignItems: 'center',
             ":focus": {
                 outline: 'none'
+            },
+            width: '60%',
+            [theme.breakpoints.down('md')]: {
+                width: '70%',
+            },
+            [theme.breakpoints.down('sm')]: {
+                width: '90%',
             },
 
         },
@@ -29,6 +38,12 @@ const imageModalStyles = makeStyles()((theme) => {
         modalImage: {
             width: '23rem',
             margin: theme.spacing(2, 0, 2, 0),
+            [theme.breakpoints.down('md')]: {
+                width: '19rem',
+            },
+            [theme.breakpoints.down('sm')]: {
+                width: '10rem',
+            },
         }
     }
 })
