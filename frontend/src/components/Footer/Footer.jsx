59d6b236d1d9d98bfacc8798dd9c82571bb07337
Yakeen Kapali <yakeenkapali@gmail.com>
2026-04-12 22:30:11 +0545

    fix: adds footer social icons and links

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,33 +1,56 @@
-import { Box, IconButton, Typography, Link } from "@mui/material"
-import footerStyles from "../../styles/footerStyles"
-import InstagramIcon from '@mui/icons-material/Instagram'
-import FacebookIcon from '@mui/icons-material/Facebook'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import { Box, IconButton, Typography, Link } from "@mui/material";
+import footerStyles from "../../styles/footerStyles";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
-    const { classes } = footerStyles()
+  const { classes } = footerStyles();
 
-    return (
-        <Box className={classes.footer}>
-            <Box>
-                <Link href="#home" variant="h4" className={classes.footerTitle}>yakeenkapali.</Link>
-            </Box>
-            <Box className={classes.footerSocials}>
-                <IconButton href="https://instagram.com/paubha_" target="_blank" rel="noopener">
-                    <InstagramIcon />
-                </IconButton>
-                <IconButton href="https://www.facebook.com/paubharts" target="_blank" rel="noopener">
-                    <FacebookIcon />
-                </IconButton>
-                <IconButton href="https://www.facebook.com/paubharts" target="_blank" rel="noopener">
-                    <LinkedInIcon />
-                </IconButton>
-            </Box>
-            <Box className={classes.footerCopyrights}>
-                <Typography variant="outline">&copy; {new Date().getFullYear()} yakeenkapali. All rights reserved</Typography>
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box className={classes.footer}>
+      <Box>
+        <Link href="#home" variant="h4" className={classes.footerTitle}>
+          yakeenkapali.
+        </Link>
+      </Box>
+      <Box className={classes.footerSocials}>
+        <IconButton
+          href="https://instagram.com/paubha_"
+          target="_blank"
+          rel="noopener"
+        >
+          <InstagramIcon />
+        </IconButton>
 
-export default Footer
+        <IconButton
+          href="https://dribbble.com/yakeenkapali"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            src="/dribbble.svg"
+            alt="Dribbble"
+            width={22}
+            height={22}
+            style={{ filter: "invert(1)" }}
+          />
+        </IconButton>
+
+        <IconButton
+          href="https://www.linkedin.com/in/yakeenkapali/"
+          target="_blank"
+          rel="noopener"
+        >
+          <LinkedInIcon />
+        </IconButton>
+      </Box>
+      <Box className={classes.footerCopyrights}>
+        <Typography variant="outline">
+          &copy; {new Date().getFullYear()} yakeenkapali. All rights reserved
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
